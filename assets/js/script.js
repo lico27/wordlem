28f63d4b50e16b03dbf221fa5cefbb1f1286b8e3
Liam Cottrell <lemcottrell@gmail.com>
2024-01-12 21:15:45 +0000

    start building event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,16 @@ let kbRow3 = $("#kb-row3");
 function makeKeyboard() {
     for (let i = 0; i < 10; i++) {
         kbRow1.append("<button class='key' id='" + letters[i] + "'>" + letters[i] + "</button>");
-        
+        let key = $("#" + letters[i]);
+        let count1 = 0;
+        key.on("click", function (event) {
+            console.log(letters[i]);
+            count1++;
+            $('.tile:empty:first').text(letters[i]);
+            if (count1 >= 5) {
+                $(this).off(event);
+              }
+        });
       };
 
     for (let j = 10; j < 19; j++) {
@@ -21,12 +30,12 @@ function makeKeyboard() {
       };
 
     for (let k = 19; k < 26; k++) {
-        kbRow3.append("<button class='key' id='" + letters[k] + "'>" + letters[k] + "</button>"); 
+        kbRow3.prepend("<button class='key' id='" + letters[k] + "'>" + letters[k] + "</button>"); 
         
-    }  
+    };
+
+    kbRow3.prepend("<button class='key-wide' id='Enter'>ENTER</button>");
+    kbRow3.append("<button class='key-wide' id='Delete'>" + "<i class='fas fa-delete-left fa-2x'></i>" + "</button>")
 };
 
 makeKeyboard();
-
-
-
