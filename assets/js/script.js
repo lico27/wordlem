ee60d83b9245f6b5424526778ba205c57b99b5d9
Liam Cottrell <lemcottrell@gmail.com>
2024-08-07 16:42:57 +0100

    fix keyboard bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-// Build keyboard
-const letters = [
-    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
-    "A", "S", "D", "F", "G", "H", "J", "K", "L",
-    "Z", "X", "C", "V", "B", "N", "M"
-  ];
+// // Build keyboard
+// const letters = [
+//     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+//     "A", "S", "D", "F", "G", "H", "J", "K", "L",
+//     "Z", "X", "C", "V", "B", "N", "M"
+//   ];
 
-  function makeKeyboard() {
-    for (let i = 0; i < letters.length; i++) {
-        $('.key:empty:first').text(letters[i]).attr("id", letters[i]);
-    };
-  };
+//   function makeKeyboard() {
+//     for (let i = 0; i < letters.length; i++) {
+//         $('.key:empty:first').text(letters[i]).attr("id", letters[i]);
+//     };
+//   };
 
-  makeKeyboard();
+//   makeKeyboard();
 
 
