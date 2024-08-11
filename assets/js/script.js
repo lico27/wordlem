ed5292006c36f47cc88641014584b9c069d24772
Liam Cottrell <lemcottrell@gmail.com>
2024-08-11 13:24:54 +0100

    make clickable function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 
+const letters = [
+  "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+  "A", "S", "D", "F", "G", "H", "J", "K", "L",
+  "Z", "X", "C", "V", "B", "N", "M"
+];
 
+  // Limit clicks to 5
+            // if (count >= 5) {
+            //     $(this).off(event);
+            //   };
 
+// make keys clickable and append letters to tiles
+for (let i = 0; i < letters.length; i++) {
+  let key = $("#" + letters[i]);
+  key.on("click", function (event) {
+  wordChoice.push(letters[i]);
+  $('.tile:empty:first').text(letters[i]);
+
+});
+  
+}
 
 
 
@@ -14,11 +33,11 @@
 
 
 // Make keyboard
-const letters = [
-    "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
-    "A", "S", "D", "F", "G", "H", "J", "K", "L",
-    "Z", "X", "C", "V", "B", "N", "M"
-  ];
+// const letters = [
+//     "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+//     "A", "S", "D", "F", "G", "H", "J", "K", "L",
+//     "Z", "X", "C", "V", "B", "N", "M"
+//   ];
 
 let kbRow1 = $("#kb-row1");
 let kbRow2 = $("#kb-row2");
