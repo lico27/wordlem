f35ab3413744f55f937ff03eed0a24ff7b06f766
Liam Cottrell <lemcottrell@gmail.com>
2024-08-12 20:34:41 +0100

    remove limit on unique clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,28 @@ const letters = [
 ];
 
 let count = 0; // Total clicks counter
-let wordChoice = []; // Array to keep track of clicked letters
-let lettersClicked = new Set(); // Set to keep track of letters clicked
+let letterArray = []; // Array to keep track of clicked letters
 
 for (let i = 0; i < letters.length; i++) {
   let key = $("#" + letters[i]);
   key.on("click", function (event) {
+    // Push the clicked letter to the array
+    letterArray.push(letters[i]);
+    $('.tile:empty:first').text(letters[i]);
+    count++;
 
-    // Check if this letter has already been clicked
-    if (!lettersClicked.has(letters[i])) {
-      wordChoice.push(letters[i]);
-      $('.tile:empty:first').text(letters[i]);
-      lettersClicked.add(letters[i]);
-      count++;
+    // Convert the array to a string
+    let wordChoice = letterArray.join("");
+    console.log("wordChoice:", wordChoice);
 
-      // Stop all keys from being clickable after 5 clicks
-      if (count >= 5) {
-        $(".key").off("click");
-      }
-
-      // Stop this key from being clickable again
-      $(this).off("click");
+    // Stop all keys from being clickable after 5 clicks
+    if (count >= 5) {
+      $(".key").off("click");
     }
   });
 }
 
-console.log(wordChoice);
+
+// check if word exists in English
+
+// https://api.dictionaryapi.dev/api/v2/entries/en/<word>
